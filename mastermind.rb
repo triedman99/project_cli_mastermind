57d361a32b3cfc29216294f23b8c8cc92f8da431
Tamal Riedman <triedman99@gmail.com>
2016-08-21 23:14:03 -0700

    Complete human as codebreaker.

--- mastermind.rb
+++ mastermind.rb
@@ -1,21 +1,33 @@
-require_relative 'board.rb'
-require_relative 'player.rb'
-require_relative 'codebreaker.rb'
+#require_relative 'board.rb'
+#require_relative 'player.rb'
+#require_relative 'codebreaker.rb'
 #require_relative 'codemaker.rb'
 
 class Mastermind
 
-  attr_reader :turn, :user_guess, :colors, :code, :guess_array
+  attr_reader :turn, :user_guess, :colors, :code, :guess_array, :players
 
   def initialize
     @turn = 0
-    @board = Board.new
-    @user_guess = ""
-    #@codemaker = Codemaker.new
+    @board = []
+    @user_guess = []
     @colors = ['red', 'green', 'blue', 'yellow']
     @code = []
     @guess_array = []
+    @players = 'codebreaker'
   end
+
+  #def players?
+  #  puts "# Enter whether you would like to play as the codemaker or codebreaker."
+  #  puts "# ----->"
+  #  @players = gets.chomp.downcase
+  #  if @players == 'codebreaker'
+  #    create_code
+  #  elsif @players == 'codemaker'
+  #    puts "# You will need to make a secret code. Choose four colors from Red, Green, Blue, Yellow. Enter them one at a time:"
+  #    get_code
+  #  end
+  #end
 
   def create_code
     4.times do
@@ -23,6 +35,14 @@ class Mastermind
     end
     @code
   end
+
+  #def get_code
+  #  4.times do
+  #    user_code = gets.chomp.downcase
+  #    @code << user_code
+  #  end
+  #  @code
+  #end
 
   def greet
     puts "# Welcome to Mastermind!"
@@ -43,48 +63,69 @@ class Mastermind
     puts "# What's your guess?"
     get_guesses
     puts "# You guessed #{@guess_array}"
+    #puts "\n The code is #{@code} \n"
+    check_guess
   end
 
   def get_guesses
     4.times do |i|
-      user_guess = gets.chomp.downcase
-      @guess_array << user_guess
+      @user_guess = gets.chomp.downcase
+      @guess_array << @user_guess
     end
   end
 
   def win?
-    if @guess_array == @code
+    false
+    if @guess_array[-4..-1] == @code
       puts "# You won! Great Job!"
       true
-    else
+    elsif @guess_array[-4..-1] != @code && @turn >= 12
       puts "# You lost. Better luck next time."
       puts "# The code was #{@code}"
-      false
+      true
     end
   end
 
   def game_over?
-    if win? || !win? && (@turn == 12)
+    if win?
       exit
     end
   end
 
   def turn
-    counter = 0
     while @turn < 12 do
-      prompt
-      @board[counter] << prompt
-      counter += 1
+      @turn += 1
+      @board << @user_guess
       game_over?
-      @board.display_board
+      prompt
     end
+  end
+
+  def check_guess
+    exact = 0
+    near = 0
+    @guess_array[-4..-1].each_with_index do |choice, position|
+      if exact?(choice, position)
+        exact += 1
+      elsif near?(choice)
+        near += 1
+      end
+    end
+    puts "# You had #{exact} exact match(es) and #{near} near match(es)."
+  end
+
+  def exact?(choice, position)
+    choice == @code[position]
+  end
+
+  def near?(choice)
+    @code.include?(choice)
   end
 
   def play
     greet
+    #players
     create_code
-
-
     start
     prompt
     turn
